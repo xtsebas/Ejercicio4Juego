7534a4d62c708ec2f9b496589bf067f6ec7d9fad
Sebastian <thiagohugo2014@gmail.com>
2022-10-02 15:55:34 -0600

    Se crearon clases nuevas

---
+++ Controller/Explorer.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import java.util.Scanner;
+
+public class Explorer extends Player{
+    Scanner in= new Scanner(System.in);
+    public Explorer(String name, int weapon, int hability, int life){
+        super(name, weapon, hability, life);
+        setLife(100);
+        System.out.println("Por elegir explorador, sus armas tienen ataques especiales, cual desea? \n1. Egolas(arco) \nDaño: 20 \nGanancia de" +
+                "vida con daño hecho: 20 \n2. Griffith(mazo) \nDaño: 50 \nGanancia de vida con daño hecho: 15 \n3.Lighting(magia) \nDaño: 10 \nGanancia de vida con daño hecho: 30");
+        setWeapon(in.nextInt());
+        try {
+            if (getWeapon()<=3 && 0>getWeapon()){
+                System.out.println("Ingreso de arma correcto");
+            }
+        }catch (Exception e){
+            System.out.println("Ingreso un numero incorrecto " + e.getMessage());
+        }
+        in.nextLine();
+        System.out.println("Eligir tu habilidad de guerrero: \n1.Curacion: agrega 35 puntos a su vida\n2.Ojo de halcon: Agrega 20 puntos en ataque \n3.Yisus: Daño agregado al ataque: 15 ");
+        setHability(in.nextInt());
+        try {
+            if (getHability()<=3 && 0>getHability()){
+                System.out.println("Ingreso de arma correcto");
+            }
+        }catch (Exception e){
+            System.out.println("Ingreso un numero incorrecto " + e.getMessage());
+        }
+    }
+
+}
---
+++ Controller/Warrior.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.util.Scanner;
+
+public class Warrior extends Player{
+    Scanner in= new Scanner(System.in);
+
+    public Warrior(String name, int weapon, int hablity, int life){
+        super(name, weapon, hablity, life);
+        setLife(100);
+        System.out.println("Por elegir guerrero, sus armas (espadas) tienen ataques especiales, cual desea? \n1. Excalibur \nDaño: 40 \nGanancia de" +
+                "vida con daño hecho: 10 \n2. Chunchunmaru \nDaño: 50 \nGanancia de vida con daño hecho: 5 \n3. Steve \nDaño: 20 \nGanancia de vida con daño hecho: 25");
+        setWeapon(in.nextInt());
+        try {
+            if (getWeapon()<=3 && 0>=getWeapon()){
+                System.out.println("Ingreso de arma correcto");
+            }
+        }catch (Exception e){
+            System.out.println("Ingreso un numero incorrecto " + e.getMessage());
+        }
+        in.nextLine();
+        System.out.println("Eligir tu habilidad de guerrero: \n1.Fuerza: agrega 10 puntos a su ataque \n2.Resistencia: Agrega 20 puntos en vida \n3.Bola de fuego: Daño agregado al ataque: 25 ");
+        setHability(in.nextInt());
+        try {
+            if (getHability()<=3 && 0>=getHability()){
+                System.out.println("Ingreso de arma correcto");
+            }
+        }catch (Exception e){
+            System.out.println("Ingreso un numero incorrecto " + e.getMessage());
+        }
+
+    }
+
+}
